6aba43e481bcea7a86005c0fa6e1bbaf56c629c9
Will Matheus <dev.owillmatheus@gmail.com>
2023-03-02 18:07:37 -0300

    Task 8 Atualizada

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -10,8 +10,8 @@ function Home(){
  <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo' >Eu sou Willian Matheus!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>Bora bater um papo? </Typography>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo' >VAGALUME</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>A sua lavanderia em qualquer lugar! </Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
@@ -22,7 +22,7 @@ function Home(){
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
-                    <img src="https://github.com/OWillMatheuz.png" alt="" width="200px" height="200px"/>
+                    <img src="https://cdn.discordapp.com/attachments/1064895158736408576/1080951240118255746/pngtree-laundry-icon-and-symbol-template-png-image_1853157-removebg-preview.png" alt="" width="500px" height="500px"/>
                 </Grid>
                 <Grid xs={12} className='postagens'>
                 </Grid>
