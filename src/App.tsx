11dde8713f7ce875da5ececd80438c3ff9fede77
Guilherme S.Fernandes <guilherme.fernandes.sou@gmail.com>
2023-03-13 15:49:40 -0300

    Task-11 Deletar Postagem e Alteração Cadastro Tema

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Login from './paginas/login/Login';
+import Cadastro from './paginas/cadastro/Cadastro';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import Home from './paginas/home/Home';
+import './App.css';
+import Lavanderias from './paginas/lavanderias/Lavanderias';
+import ListaTema from './components/tema/listatema/ListaTema';
+
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar />
+        <div style={{ minHeight: '100vh' }}>
+          <Routes> // Antigo Switch
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/cadastrolavanderia" element={<Cadastro />} />
+            <Route path="/home" element={<Home />} /> 
+            <Route path="/lavanderias" element={<Lavanderias />} />
+            <Route path="/temas" element={<ListaTema />} />     
+          </Routes>
+        </div>
+          <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
